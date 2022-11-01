4e7e435906ea79e36947655afa9f1f5a0c44aeff
Korpo-spec <casper22x@gmail.com>
2022-11-01 14:41:58 +0100

    Bouncy balls

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+
+    [SerializeField] private float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         Vector2 vec = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
         vec.randomVec2();
-        rb.velocity = vec.normalized;
+        rb.velocity = vec.normalized * speed;
     }
 
     // Update is called once per frame
